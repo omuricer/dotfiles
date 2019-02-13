443f308540f9e2eea2f89cc059424b87281738ce
omuricer <inada@PC11598.usengroup.com>
2019-02-13 18:04:09 +0900

    aaa

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# $1:windowsのユーザー名
+if [ "$1" = "" ]
+then
+    echo "no argument"
+fi
+
+# ホーム配下をバックアップ
+mkdir $HOME/_dotfilesbk
+now=`date '+%Y%m%d%H%M%S'`
+mkdir $HOME/_dotfilesbk/$now
+rsync -a $HOME/ $HOME/_dotfilesbk/$now --exclude='dotfiles' --exclude='_dotfilesbk'
+
+WINDOWS_HOME=/mnt/c/Users/$1
+
+
 DOT_FILES=( \
 .bash_history \
 .bash_logout \
