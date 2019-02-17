5cfb5aeaacc78e47498a5bcb6a3ef4dbd93c044c
omuricer <omuricer@inac.local>
2019-02-17 20:03:18 +0900

    fish theme.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,5 @@ set -g theme_display_git_master_branch yes
 set -g theme_title_display_process yes
 set -g theme_display_user ssh
 set -g theme_git_worktree_support yes
-set -g theme_color_scheme terminal-light-black
 set -g theme_nerd_fonts yes
 
